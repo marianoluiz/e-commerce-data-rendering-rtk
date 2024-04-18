973fbe4fd4672d48fe1719b6ab6141903690e431
Richa Arora <richaaeri9988@gmail.com>
2024-04-18 17:45:22 +0530

    package file

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
  
       <div>
-        <h1>E-Commerce Application</h1>
+        <h1 className='app-heading'>E-Commerce Application</h1>
         <ProductList />
         <ShoppingCart />
       </div>
